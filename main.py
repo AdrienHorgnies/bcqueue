6ada88db733c3a3cae7ab5711a3cbefc245f6072
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2021-11-05 09:19:45 +0100

    testing map, failure

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sys
 from numpy.random import SeedSequence, SFC64, Generator
 
 from measures import print_stats, print_graphs
-from simulations import mm1_simulation, map_ph_simulation
+from simulations import mm1_simulation, map_ph_simulation, Map
 
 SEED_SEQUENCE = SeedSequence()
 
@@ -50,7 +50,62 @@ def main():
         print_stats(arrivals, waitings, blocks)
         print_graphs(arrivals, waitings, blocks)
 
+    import numpy as np
     if args.mapph1:
+        g = generators[0]
+
+        C = np.array([
+            [-1.5, +0.5],
+            [+0.2, -1.2]
+        ])
+        D = np.array([
+            [+0.5, +0.5],
+            [+0.5, +0.5]
+        ])
+        p = np.array([0.5, 0.5])
+
+        arrival_rate = np.matmul(np.matmul(p, D), np.ones(2))
+        print(f"arrival rate : {arrival_rate}")
+
+        _map = Map(g,
+                   C=C,
+                   D=D,
+                   v=p
+                   )
+
+        t = 0
+        tau = 10 ** 3
+        print(f"expected number of arrivals : {tau * arrival_rate:.0f}")
+
+        def next_arrival():
+            nonlocal t
+            t += g.exponential(- (_map.C[_map.state][_map.state]))
+
+            weights = np.concatenate((_map.C[_map.state], _map.D[_map.state]))
+            weights[weights < 0] = 0
+            probabilities = weights / weights.sum()
+
+            next_state = g.choice(list(range(len(probabilities))), 1, p=probabilities)[0]
+
+            if next_state < len(_map.C):
+                _map.state = next_state
+                return next_arrival()
+            else:
+                _map.state = next_state - len(_map.C)
+                return t
+
+        tx_count = 0
+
+        _map.roll_state()
+
+        while t < tau:
+            next_arrival()
+
+            tx_count += 1
+
+        print(f"simulated number of arrival : {tx_count}")
+
+        return
         arrivals, waitings, blocks = map_ph_simulation(generators, tau=1000 * 600,
                                                        C=[[-1.5, 0.5],
                                                           [0.2, -1.2]],
